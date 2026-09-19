2fb0c5b1925a32c83515c396c10cf2d632d049c0
atharvparmar01-prog <atharvparmar01@gmail.com>
2026-09-19 19:07:41 +0530

    Simple calculator program

--- test.java
+++ test.java
@@ -1,5 +1,27 @@
 class test{
     public static void main(String[] args){
-        System.out.print("Hello");
+        calculator opreration = new calculator(3,2);
+        opreration.sub();
+    }
+}
+
+class calculator{
+    int num1;
+    int num2;
+    calculator(int a,int b){
+        num1 = a;
+        num2 = b;
+    }
+    void add(){
+        System.out.println("Addition : " + (num1+num2));
+    }
+    void sub(){
+        System.out.println("Subration : " + (num1-num2));
+    }
+    void multi(){
+        System.out.println("Multiplication : " + (num1*num2));
+    }
+    void divison(){
+        System.out.println("Divison : " + (num1/num2));
     }
 }
